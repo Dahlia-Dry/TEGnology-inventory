da0852992969128d6ee3aaa9df994c3f91656c02
Dahlia_Dry <dahlia.dry@alum.mit.edu>
2023-11-15 10:10:32 +0100

    update settings

--- inventory/settings.py
+++ inventory/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$eu!3i_ayv!1+el-yai&zmof*(lzcuh9539psd&04yxl*ug9s$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+            '.tegnology.pythonanywhere.com',
+            '127.0.0.1']
 
 # Application definition
 
